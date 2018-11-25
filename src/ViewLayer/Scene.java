0bd22926673145523f11c8883cd6ef858d0b22b6
lyx937131777 <937131777@qq.com>
2018-11-25 23:58:40 +0800

    menubar

--- src/ViewLayer/Scene.java
+++ src/ViewLayer/Scene.java
@@ -10,6 +10,8 @@ public class Scene extends JFrame
 {
     private static final long serialVersionUID = 1L;// serialVersionUID唯一的可串行化的版本
     private GeoRender geoRender = null;
+    private JMenuItem load;
+    private JMenuItem save;
 
     public Scene(JPanel jp)
     {
@@ -18,8 +20,37 @@ public class Scene extends JFrame
         this.setSize(1300, 720);
         this.setLocationRelativeTo(null);
         this.add(jp);
+
+        initialise();
         this.setVisible(true);
 
     }
 
+    private void initialise() {
+       addMenuBar();
+    }
+
+    private void addMenuBar() {
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+
+        JMenuItem newBoard = new JMenuItem("New");
+        //newBoard.addActionListener(listeners.get("nBL"));
+        load = new JMenuItem("Load");
+        //load.addActionListener(listeners.get("lL"));
+        save = new JMenuItem("Save");
+        //save.addActionListener(listeners.get("sL"));
+        JMenuItem exit = new JMenuItem("Exit");
+        //exit.addActionListener(listeners.get("eL"));
+
+        menu.add(newBoard);
+        menu.add(load);
+        menu.add(save);
+        menu.addSeparator();
+        menu.add(exit);
+
+        menuBar.add(menu);
+        this.setJMenuBar(menuBar);
+    }
 }
